7d0a105d85c07662d39822cbe8cac5f021119b8f
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-11 17:11:51 +0500

    completed Intro component layout

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,7 +3,23 @@ import "./intro.scss";
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="../assets/myPhoto.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>
+            Hi! <br />
+            I'm Alexandr <br />a WEB developer <br />
+            (but you call name Sasha)
+          </h2>
+        </div>
+        <a href="#portfolio">
+          <img src="../../assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
