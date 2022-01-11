6f094531e5815be762f1f4600e63fcb3304c5504
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-11 09:18:25 +0500

    Установил иконки MUI, сверстал часть header

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,5 +1,25 @@
 import "./topbar.scss";
-
+import { Phone, Mail } from "@material-ui/icons";
 export default function Topbar() {
-  return <div className="topbar">topbar</div>;
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            AlexDev
+          </a>
+          <div className="itemContainer">
+            <Phone className="icon" />
+            <span>+79022552132</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>AlexanderIlinykhDev@yandex.ru</span>
+          </div>
+        </div>
+
+        <div className="right">right</div>
+      </div>
+    </div>
+  );
 }
